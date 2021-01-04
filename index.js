d1c8e7494ce34a205498a33303aade988c34afcd
saketh2341 <54940770+saketh2341@users.noreply.github.com>
2021-01-05 00:39:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+const express=require('express')
+const app=express()
+const path=require('path')
+const ejs=require('ejs')
+const flash=require('express-flash')
+const expressLayout=require('express-ejs-layouts')
+const session=require('express-session')
+const mongoose=require('./app/models/mongoose')
+const MongoDbStore=require('connect-mongo')(session)
+const passport = require('passport')
+const Emitter = require('events')
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+const connection = mongoose.connection;
+
+
+
+// Session store
+let mongoStore = new MongoDbStore({
+                mongooseConnection: connection,
+                collection: 'sessions'
+            })
+
+    const eventEmitter = new Emitter()
+    app.set('eventEmitter', eventEmitter)
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store:mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+}))
+
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    res.locals.user = req.user
+    next()
+})
+
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+require('./routes/web')(app)
+app.use((req, res) => {
+    res.status(404).render('errors/404')
+})
+
+
+const PORT=process.env.PORT || 3000
+const server=app.listen(PORT,()=>{
+    console.log('server started')
+})
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+      // Join
+      socket.on('join', (orderId) => {
+        socket.join(orderId)
+      })
+})
+
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data)
+})
+
+eventEmitter.on('orderPlaced', (data) => {
+
+    io.to('adminRoom').emit('orderPlaced', data)
+})
+
+eventEmitter.on('cartCount',(data)=>{
+    io.to('cartcounter').emit('cartCount',data)
+})
+
